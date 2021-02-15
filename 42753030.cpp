9e5c80bfb8d65cf1be8f81f471bc14c21cbe03b8
siddisgreat <78675340+siddisgreat@users.noreply.github.com>
2021-02-16 01:13:31 +0530

    Add files via upload

---
+++ 42753030.cpp
@@ -0,0 +1,15 @@
+/*
+    AUTHOR : SIDDHANT MANZE
+    DATE   : 14/02/2021
+*/
+#include<iostream>
+#define ll long long
+using namespace std;
+
+int main()
+{
+    ll n;
+    cin>>n;
+    cout<<n;
+    return 0;
+}
